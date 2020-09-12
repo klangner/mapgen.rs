dbf765dff45fc46b3885f32ca51fc6cdc9749ab0
klangner <klangner@gmail.com>
2020-09-12 15:10:35 +0200

    working webpack

--- demo/README.md
+++ demo/README.md
@@ -1,6 +1,10 @@
 # Demo application for mapgen
 
+## Build the project:
 
+```
+wasm-pack build
+```
 
 This app uses:
   * [Urizen OneBit Tilesets](https://vurmux.itch.io/urizen-onebit-tilesets)
--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -11,7 +11,7 @@ pub fn main() -> Result<(), JsValue> {
 
     // Manufacture the element we're gonna append
     let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+    val.set_inner_html("Hello from Rust! 1");
 
     body.append_child(&val)?;
 
