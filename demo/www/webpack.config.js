633ec7a535db0e981810286a6c68dea4a19c2e2d
klangner <klangner@gmail.com>
2020-10-17 22:27:05 +0200

    copy assets with webpack

--- demo/www/webpack.config.js
+++ demo/www/webpack.config.js
@@ -9,6 +9,6 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html', 'assets'])
+    new CopyWebpackPlugin(['index.html', { from: 'assets', to: 'assets' }])
   ],
 };
